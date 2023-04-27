b5751d51246de1450a47416ffa4bb1bca7fb8c1c
عبدالوهاب <abdolvahab88@yahoo.com>
2023-04-27 11:56:13 +0430

    feat: add environment variables for development and production chore: add files to .gitignore feat: add ConfirmCode, LoginPage, and SuccessPage components feat: add auth-context and use-auth hooks refactor: use react-router-dom for routing chore: remove jest-dom import from setupTests.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
+import App from "App";
+import {ConfirmCode} from "components/confirmCode";
+import {LoginPage} from "components/loginPage";
+import {SuccessPage} from "components/successPage";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import './index.sass';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {path: '/login-page', element: <LoginPage />},
+  {path: '/confirm-code', element: <ConfirmCode />},
+  {
+    path: '/', element: <App />, children: [
+      {path: 'success-page', element: <SuccessPage />}
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.unregister();
 reportWebVitals();
